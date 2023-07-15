bf1660f8989911689d0dbb6c1924f832a40340d0
sureface <jasurdjuraev2002@gmail.com>
2023-07-15 23:53:31 +0500

    project ready to build

---
+++ src/api/server.jsx
@@ -0,0 +1,54 @@
+// const models = [
+//     {
+//         tables: "Tables",
+//         subMenu: {
+//             TableSureface: {
+//                 gray: 1,
+//                 yellow: 2,
+//                 brown: 3
+//                 silver: 4
+//             },
+//             legs: {
+//                 square: 1,
+//                 threeCorner: 2,
+//                 simple: 3,
+//                 unreal: 4
+//             }
+//         }
+//     },
+// ]
+
+// {
+//     tables: "Tables",
+//     subMenu: {
+//         TableSureface: {
+//             gray: 1,
+//             yellow: 2,
+//             brown: 3
+//             silver: 4
+//         },
+//         legs: {
+//             square: 1,
+//             threeCorner: 2,
+//             simple: 3,
+//             unreal: 4
+//         }
+//     }
+// },
+// {
+//     tables: "Chairs",
+//     subMenu: {
+//         Chairs: {
+//             gray: 1,
+//             yellow: 2,
+//             brown: 3
+//             silver: 4
+//         },
+//         legs: {
+//             square: 1,
+//             threeCorner: 2,
+//             simple: 3,
+//             unreal: 4
+//         }
+//     }
+// }
